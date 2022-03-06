5b965180f295d4a78c41e38059cacbe36e61ad61
canal <5557891+v2canal@user.noreply.gitee.com>
2022-03-06 10:50:39 +0800

    nuxt目录

---
+++ plugins/element-ui.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.config.productionTip = false
+
+Vue.use(Antd)
+Vue.use(Element, { locale })
